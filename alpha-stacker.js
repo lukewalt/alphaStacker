9912128f42f79441280b293fd206054353760c63
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-21 16:21:52 -0600

    added logic to log contents of array per instance of for loop

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,12 +1,15 @@
 // Create an array that contains the letters of the alphabet
-var alphabet = [];
+var alphabet = [ "a", "b", "c", "d", "e", "f" ];
 
 // The stackLetter function should accept the array as the sole argument
-function stackLetters (theAlphabetArray) {
-    /*
-      Write a `for` loop that iterates over the array argument and
-      outputs the letters.
-     */
+function stackLetters (alphabet) {
+  /*
+    Write a `for` loop that iterates over the array argument and
+    outputs the letters.
+   */
+  for(var i = 0; i < alphabet.length; i++) {
+    console.log(alphabet[i]);
+  }
 }
 
 // Invoke the function and pass in the array
