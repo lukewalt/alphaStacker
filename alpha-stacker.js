4379975f57b1f6f3baabd0b3627c487e6b621cbf
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-21 21:36:03 -0600

    added variable to coalesce each instance of the array

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -2,14 +2,17 @@
 var alphabet = [ "a", "b", "c", "d", "e", "f" ];
 
 // The stackLetter function should accept the array as the sole argument
-function stackLetters (alphabet) {
+function stackLetters (theAlphabetArray) {
   /*
     Write a `for` loop that iterates over the array argument and
     outputs the letters.
    */
+  var row = "";
   for(var i = 0; i < alphabet.length; i++) {
-    console.log(alphabet[i]);
+    var row = row + alphabet[i];
+    console.log(row);
   }
+  
 }
 
 // Invoke the function and pass in the array
